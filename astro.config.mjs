2ca13304ed8693434f7545b4b73914823331fddb
Danilo Nobre <danilonobren@gmail.com>
2024-02-23 00:51:06 -0300

    base layouts, content and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import solidJs from "@astrojs/solid-js";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://dankonobre.github.io',
   base: '/',
-  integrations: [tailwind()]
+  output: 'static',
+  integrations: [,
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }),
+    mdx(),
+    solidJs(),
+  ],
+  vite: {
+    types: ["vite/client"]
+  }
 });
