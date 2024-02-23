277c5bf36fdadd73a37ac899897fd131c2eb3896
Danilo Nobre <danilonobren@gmail.com>
2024-02-23 00:05:31 -0300

    starting layouts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     './src/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './**/*.scss'
   ],
   theme: {
     extend: {
