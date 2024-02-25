cffd6cab4e6fe68d83e13a5a20bd959d37234034
Danilo Nobre <danilonobren@gmail.com>
2024-02-25 11:41:30 -0300

    packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,30 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
+import metaTags from "astro-meta-tags";
+import react from "@astrojs/react";
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
+import lighthouse from "astro-lighthouse";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://dankonobre.github.io',
   base: '/',
   output: 'static',
-  integrations: [,
-    tailwind({
-      config: {
-        applyBaseStyles: false
-      }
-    }),
-    mdx(),
+  integrations: [, tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx(),
     solidJs(),
+    metaTags(),
+    react(),
+    sentry(),
+    spotlightjs(),
+    lighthouse(),
   ],
   vite: {
     types: ["vite/client"]
